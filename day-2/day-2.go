15c2c3cf94430e1864443187f4b1bed6265cc062
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@gmail.com>
2024-12-02 10:59:31 +0100

    Add day 2 part 2

--- day-2/day-2.go
+++ day-2/day-2.go
@@ -3,7 +3,6 @@ package main
 import (
 	"aoc-2024/common"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -33,46 +32,89 @@ func readLines(fileName string) [][]int{
 	return reports
 }
 
+func getDiffMaps(report []int) (map[int]int, map[int]int) {
+	posDiffMap := make(map[int]int)
+	negDiffMap := make(map[int]int)
+
+	for idx, value := range report[:len(report) - 1] {
+		diff := report[idx+1] - value 
+
+		if diff >= 0 {
+			posDiffMap[diff]++
+		}
+
+		if diff < 0 {
+			negDiffMap[diff]++
+		}
+	}
+
+	return posDiffMap, negDiffMap
+}
+
+func isValid(report []int) bool {
+	posDiffMap, negDiffMap := getDiffMaps(report)
+
+	// Only one of these should be non empty
+	if len(posDiffMap) > 0 && len(negDiffMap) > 0 {
+		return false
+	}
+
+	for k := range posDiffMap {
+		if k == 0 || k > 3 {
+			return false
+		}
+	}
+
+	for k := range negDiffMap {
+		if k == 0 || k < -3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+
 func part1(fileName string) int {
 	reports := readLines(fileName)
 	safeReports := 0
 
-	for idx, report := range reports {
-		diff := make([]int, len(report) - 1)
-		
-		// Calculate the difference between sequential values in the report
-		for idx, value := range report[:len(report) - 1] {
-			nextValue := report[idx + 1]
-			diff[idx] = nextValue - value
-		}
-
-		sign := math.Signbit(float64(diff[0]))
-		safe := true
-
-		for _, value := range diff {
-			// If the sign is different than the previous value, the report is not safe
-			if sign != math.Signbit(float64(value)) {
-				safe = false
-				break
-			}
-
-			// If the absolute difference between both values is greater than 3 or smaller than 1, the report is not safe
-			if math.Abs(float64(value)) > 3 || math.Abs(float64(value)) < 1 {
-				safe = false
-				break
-			}
-		}
-
-		if safe {
+	for _, report := range reports {
+		if isValid(report) {
 			safeReports++
-		}
+		} 
 	}
 
 	return safeReports
 }
 
 func part2(fileName string) int {
-	return 0
+	reports := readLines(fileName)
+	safeReports := 0
+
+	for _, report := range reports {
+		validSubReports := 0
+
+		if isValid(report) {
+			safeReports++
+			continue
+		}
+
+		for idx := 0; idx < len(report); idx++ {
+			copiedReport := make([]int, len(report))
+			copy(copiedReport, report)
+			erasedReport := append(copiedReport[:idx], copiedReport[idx+1:]...)
+			if isValid(erasedReport) {
+				validSubReports++
+			}
+		}
+
+		if validSubReports > 0 {
+			safeReports++
+		}
+	}
+
+	return safeReports
 }
 
 func main() {
